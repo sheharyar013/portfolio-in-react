4e5138384dc54e15b3cd3616749118119f4206d3
Harry <harrymian@scoutsecure.com>
2024-02-05 09:08:01 -0500

    code pushed

---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,24 @@
+import { BsLinkedin } from "react-icons/bs";
+import { FaGithub } from "react-icons/fa";
+import { FiDribbble } from "react-icons/fi";
+
+const HeaderSocials = () => {
+  return (
+    <div className="header__socials">
+      <a
+        href="https://www.linkedin.com/in/sheharyar-butt-7bb01718b/"
+        target="_blank"
+      >
+        <BsLinkedin />
+      </a>
+      <a href="https://github.com/sheharyar013" target="_blank">
+        <FaGithub />
+      </a>
+      <a href="https://dribbble.com" target="_blank">
+        <FiDribbble />
+      </a>
+    </div>
+  );
+};
+
+export default HeaderSocials;
